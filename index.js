07b7b6c81ada32cd321ad22dd7550fd90c8347b9
Fernando Nicolas <fersanti2896@gmail.com>
2022-03-30 14:18:43 -0600

    Configurando servidor express

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
-console.log('Hola desde Node!');
+const express = require('express');
+
+/* Creando el Servidor/Aplicacion de Express */
+const app = express();
+
+/* Peticion GET */
+app.get('/', ( req, res ) => {
+    res.json({
+        ok: true,
+        msg: 'Todo salió bien',
+        uid: 1234
+    });
+});
+
+/* Escucha el puerto donde saldrá */
+app.listen( 4000, () => {
+    console.log(`Servidor corriendo en localhost:${4000}`);
+});
