bc4908449db46b6e812451692edc680d3d7f0de3
Fernando Nicolas <fersanti2896@gmail.com>
2022-04-01 02:21:01 -0600

    Validando campos requeridos en peticiones POST

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,31 +1,50 @@
 
 /* Haciendo tipado */
-const { response } = require('express')
+const { response } = require('express');
+const { validationResult } = require('express-validator');
 
 /* Controladores de rutas */
 const crearUsuario = ( req, res = response ) => {
+    const errors = validationResult( req );
+
+    if( !errors.isEmpty() ) {
+        return res.status(400).json({
+            ok    : false,
+            errors: errors.mapped()
+        })
+    }
+
     const { name, email, password } = req.body;
     console.log(name, email, password);
 
     return res.json({
-        ok: true,
+        ok : true,
         msg: 'Crear usuario /new'
     });
 }
 
-const loginUsuario = ( req, res ) => {
+const loginUsuario = ( req, res = response ) => {
+    const errors = validationResult( req );
+    
+    if( !errors.isEmpty() ) {
+        return res.status(400).json({
+            ok    : false,
+            errors: errors.mapped()
+        });
+    }
+
     const { email, password } = req.body;
     console.log(email, password);
 
     return res.json({
-        ok: true,
+        ok : true,
         msg: 'Login de usuario /'
     });
 }
 
-const validarToken = ( req, res ) => {
+const validarToken = ( req, res = response) => {
     return res.json({
-        ok: true,
+        ok : true,
         msg: 'Renew'
     });
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,24 @@
 /* Todas las rutas que van a tener que ver con autenticación */
 const { Router } = require('express');
+const { check } = require('express-validator');
 const { crearUsuario, loginUsuario, validarToken } = require('../controllers/auth');
 
 const router = Router();
 
 /* Para crear un usuario */
-router.post('/new', crearUsuario);
+router.post('/new', [
+    /* Middlewer para validar campos */
+    check('name', 'El nombre es requerido!').not().isEmpty(),
+    check('email', 'El correo es requerido!').isEmail(),
+    check('password', 'La contraseña es requerido!').isLength({ min: 6 })
+], crearUsuario);
 
 /* Login de usuario */
-router.post('/', loginUsuario);
+router.post('/', [
+    /* Middlewers la cual es una función normal */
+    check('email', 'El correo es obligatorio!').isEmail(),
+    check('password', 'La contraseña es obligatoria!').isLength({ min: 6 })
+], loginUsuario);
 
 /* Validando y revalidando token */
 router.get('/renew', validarToken);
